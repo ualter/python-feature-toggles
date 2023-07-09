f127447122f7a628754c135f6e39bd8a67c45386
Ualter Otoni Pereira <ualter.junior@gmail.com>
2023-07-09 11:26:38 +0200

    Improvements

--- services/dataset.py
+++ services/dataset.py
@@ -6,10 +6,7 @@ from .dataset_errors import DatasetInvalidCreation
 
 def create_dataset(context: Context, request: dict) -> Dataset:
     if context.config.is_enable(Feature.DATASET_PERMISSIONS_BY_AREA):
-        if context.user.area == "SALES" and request["storage_location"] == "EBS":
-            raise DatasetInvalidCreation("SALES are not allow to use EBS")
-        else:
-            return _create_dataset(context, request)
+        return _create_dataset_ft_perm_area(context, request)
     else:
         return _create_dataset(context, request)
 
@@ -17,3 +14,11 @@ def create_dataset(context: Context, request: dict) -> Dataset:
 def _create_dataset(context: Context, request: dict) -> Dataset:
     ds = Dataset(owner=context.user, name=request["name"], storage_location=request["storage_location"])
     return ds
+
+
+def _create_dataset_ft_perm_area(context: Context, request: dict) -> Dataset:
+    if context.user.area == "SALES" and request["storage_location"] == "EBS":
+        raise DatasetInvalidCreation("SALES are not allow to use EBS")
+    else:
+        ds = Dataset(owner=context.user, name=request["name"], storage_location=request["storage_location"])
+        return ds
